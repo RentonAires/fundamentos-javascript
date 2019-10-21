fdae4df83fbb181ff147b9e62ef0d15e3acf89b3
renton aires <rentonaires@gmail.com>
2019-10-21 09:53:20 -0300

    comit projeto fundamentos

--- funcao2.js
+++ funcao2.js
@@ -1,5 +1,24 @@
+//Armazenando uma funaçao em uma variavel
 const somaImprimida = function(a, b){
     console.log(a+b);
     
 } 
 somaImprimida(5,3)
+
+//Armazenando uma funçao arrow em uma variavel
+const soma =(a,b) => {
+    return a + b;
+}
+console.log(soma(5, 5));
+
+//Retrono implicito
+const subtracao = (a,b) => a-b;
+console.log(subtracao(5,2));
+
+//
+const imprimir = a => console.log(a);
+    imprimir('retrona implicito!!')
+
+
+
+
